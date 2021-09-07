7c957b4715ca95fd175a0730fcb6afdc627a332c
KaliCF <kalifenimore@gmail.com>
2021-09-07 15:50:04 -0400

    Kali 9/7/21

--- clientside/src/components/Music.js
+++ clientside/src/components/Music.js
@@ -1,28 +1,24 @@
 import React from "react";
 
-fetch(`https://shazam.p.rapidapi.com/search?term=stairway&locale=en-US&offset=0`, {
-  "method": "GET",
-  "headers": {
-    "x-rapidapi-host": "shazam.p.rapidapi.com",
-    "x-rapidapi-key": "cbba0d517dmsh6bc06cb82cd7cf1p107698jsn59966d2769a4"
-  }
-})
-  .then(response => {
-    return response.json();
-  })
-  .then((json) => {
-    console.log(json)
-  })
-  .catch(err => {
-    console.error(err);
-  });
-
-// displayMusic(songs) {
-//   songs.forEach(element => {
-//     console.log(element.tracks.hits)
+// fetch(`https://shazam.p.rapidapi.com/search?term=${userInput}&locale=en-US&offset=0`, {
+//   "method": "GET",
+//   "headers": {
+//     "x-rapidapi-host": "shazam.p.rapidapi.com",
+//     "x-rapidapi-key": "cbba0d517dmsh6bc06cb82cd7cf1p107698jsn59966d2769a4"
+//   }
+// })
+//   .then(response => {
+//     return response.json();
+//   })
+//   .then((json) => {
+//     console.log(json)
+//   })
+//   .catch(err => {
+//     console.error(err);
 //   });
-// }
 
-export default function Music() {
-  return <div></div>;
+export default function Music(props) {
+  return <div>
+    <p className="Title">{props.Title}</p>
+  </div>;
 }
